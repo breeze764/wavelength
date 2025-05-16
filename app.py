d3eb82140d7fce0c5fc7641ce563b9f9dcd1a4eb
breeze764 <briannajsearle@gmail.com>
2025-05-16 12:44:02 +1200

    Initial commit

---
+++ app.py
@@ -0,0 +1,49 @@
+#program to play the wavelength game without snapchat
+#by Brianna 
+#08 April 2025
+
+from flask import Flask, render_template, redirect, request, url_for
+from flask_socketio import SocketIO, emit, join_room
+from rooms import create_room, rooms
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/create', methods=['POST'])
+def create():
+    room_code = create_room()
+    return redirect(url_for('host', room_code=room_code))
+
+@app.route('/join', methods=['POST'])
+def join():
+    #gets the info from our form so they can join
+    room_code = request.form.get('room_code').upper()
+    username = request.form.get('username')
+    #adds them to the room
+    if room_code in rooms:
+        rooms[room_code]["players"].append(username)
+        #tells the server someone has joined so it can tell the host to refresh
+        socketio.emit('update_player_list',room=room_code)
+        return redirect(url_for('player', room_code=room_code, username=username))
+    else:
+        return "Room not found!", 404
+
+@app.route('/host/<room_code>')
+def host(room_code):
+    return render_template('host.html',room_code=room_code,players=rooms[room_code]["players"])
+
+@app.route('/player/<room_code>')
+def player(room_code):
+    return render_template('player.html', room_code=room_code)
+
+@socketio.on('join_room')
+def handle_join_room(room_code):
+    join_room(room_code)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
+
---
+++ rooms.py
@@ -0,0 +1,20 @@
+#program to contain more logic for the wavelength game
+#by Brianna
+#08 April 2025
+
+import random, string
+
+rooms = {}
+
+def create_room():
+    code = get_code()
+    while code in rooms:
+        code = get_code()
+    rooms[code] = {
+        "players": [],
+        "answers": []
+    }
+    return code
+
+def get_code(length=4):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
